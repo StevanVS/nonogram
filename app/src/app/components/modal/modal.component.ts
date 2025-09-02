c36897463e24779411757ab2b456946dcddbb84f
Stevan Velez <stevanvs@outlook.com>
2025-09-02 16:40:58 -0500

    Cerrar modal con clic afuera y detalles

--- app/src/app/components/modal/modal.component.ts
+++ app/src/app/components/modal/modal.component.ts
@@ -7,9 +7,29 @@ import { Component, ElementRef, input, viewChild } from '@angular/core';
   styleUrl: './modal.component.css',
 })
 export class ModalComponent {
+  canCloseOnClickOutside = input<boolean>(false);
+
   private modalEl = viewChild.required<ElementRef<HTMLDialogElement>>('modal');
 
   titleText = input.required<string>();
+
+  onDialogClick(event: MouseEvent) {
+    if (!this.canCloseOnClickOutside()) return;
+
+    const dialog = this.modalEl().nativeElement;
+    const rect = dialog.getBoundingClientRect();
+
+    // Si clic fuera del rectángulo → cerrar
+    if (
+      event.clientX < rect.left ||
+      event.clientX > rect.right ||
+      event.clientY < rect.top ||
+      event.clientY > rect.bottom
+    ) {
+      this.closeModal();
+      // console.log('cerrar')
+    }
+  }
 
   public showModal() {
     this.modalEl().nativeElement.showModal();
